fb711d6e37d36e240504b7992f411b18ff6ef4e7
ruvialexandra <ruviaranareyes@gmail.com>
2024-06-23 02:51:46 -0500

    cliente

--- src/CLIENTE/ArregloCliente.java
+++ src/CLIENTE/ArregloCliente.java
@@ -1,11 +1,15 @@
 package CLIENTE;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import Clases.Cliente;
 
 public class ArregloCliente {
 	//Campos y atributos 
 	private ArrayList <Cliente> miLista;
-	private String clienteArchivo = "cliente.txt";  
+	private String Archivostxt = "cliente.txt";  
 	
 	//Metodo constructor 
 	public  ArregloCliente () {
@@ -19,12 +23,71 @@ public class ArregloCliente {
 		return miLista.size();
 	}
 	//Metodo  obtener 
-	public Cliente obtener() {
-		return  miLista.get(i);
-		
+	public Cliente obtener(int i) {
+		return miLista.get(i);
 	}
-	//Metodo 
-	
+	//Metodo agregar 
+	public void agregar(Cliente obj) {
+		miLista.add(obj);
+	}
+	//Metodo modificar
+	public void modificar(int  i ,Cliente obj) {
+		miLista.set(i,obj);
+	}
+	//Metodo suprimir
+	public void suprimir (int i) {
+		miLista.remove(i);
+	}
+	//Metodo buscar
+	public int buscar(int codigo) {
+		for(int i = 0;i < tamaño();i++) 
+			if (obtener(i).getCodigoCliente() == codigo) return i;
+			return -1;
+		}
+
 	
 
-}
+	
+	// Metodos para la gestión de archivos , el try y catch
+		public void Grabar() {
+			try {	// Crear un archivo en disco
+					FileWriter escritor = new FileWriter(Archivostxt);
+					// Grabar la información de la lista dinamica en el archivo
+					for(int i = 0; i < tamaño(); i++)
+						escritor.write(miLista.get(i).getCodigoCliente() + ";" +
+									miLista.get(i).getNombres() + ";" +
+									miLista.get(i).getDireccion() + ";" +
+									miLista.get(i).getTelefono() +
+									miLista.get(i).getDni()+"\n");
+					// Cerrar el archivo
+					escritor.close();
+			} catch (IOException e) {
+					System.out.println("*** ERROR:" + e.getMessage());
+			}
+		}
+		
+		//Metodo leer 
+		public void Leer() {
+			String registro, campos[];
+			
+			try {
+					FileReader lector = new FileReader(Archivostxt);
+					BufferedReader lee = new BufferedReader(lector);
+					while((registro = lee.readLine()) != null) {
+						campos = registro.split(";");
+						agregar(new Cliente(Integer.parseInt(campos[0]),
+                                campos[1],
+                                campos[2],
+                                campos[3],
+                                campos[4], 
+                                campos[5]));
+					}
+					lee.close();
+			} catch (IOException e) {
+				System.out.println("*** ERROR:" + e.getMessage());
+			}		
+		}
+	}
+		
+	
+
--- src/Clases/Cliente.java
+++ src/Clases/Cliente.java
@@ -1,6 +1,64 @@
 package Clases;
 
 public class Cliente {
-	
+	// campos o atributos
+		private int codigoCliente;
+		private String nombres;
+		private String apellidos;
+		private String direccion;
+		private String telefono; 
+		private String dni;
+		
+		//Constructor vacio
+		public Cliente () {
+			
+		}
+		//Metodo constructor de todos 
+		public Cliente(int codigoCliente, String nombres, String apellidos, String direccion, String telefono,String dni) {
+			this.codigoCliente = codigoCliente;
+			this.nombres = nombres;
+			this.apellidos = apellidos;
+			this.direccion = direccion;
+			this.telefono = telefono;
+			this.dni = dni;
+		}
+		//metodo get y set
+		public int getCodigoCliente() {
+			return codigoCliente;
+		}
+		public void setCodigoCliente(int codigoCliente) {
+			this.codigoCliente = codigoCliente;
+		}
+		public String getNombres() {
+			return nombres;
+		}
+		public void setNombres(String nombres) {
+			this.nombres = nombres;
+		}
+		public String getApellidos() {
+			return apellidos;
+		}
+		public void setApellidos(String apellidos) {
+			this.apellidos = apellidos;
+		}
+		public String getDireccion() {
+			return direccion;
+		}
+		public void setDireccion(String direccion) {
+			this.direccion = direccion;
+		}
+		public String getTelefono() {
+			return telefono;
+		}
+		public void setTelefono(String telefono) {
+			this.telefono = telefono;
+		}
+		public String getDni() {
+			return dni;
+		}
+		public void setDni(String dni) {
+			this.dni = dni;
+		}
+		
 
 }
